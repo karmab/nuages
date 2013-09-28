955780610bbb37eaee4b70e0a55bac45cb93898f
karmab <karimboumedhel@gmail.com>
2013-09-28 12:58:37 +0200

    add missing js

--- static/js/allvms.js
+++ static/js/allvms.js
@@ -2,6 +2,21 @@ $(document).ready(function() {
 setInterval(function(){
   var virtualprovider = $('#id_virtualprovider').val();
   var data = { 'virtualprovider' : virtualprovider } ; 
+  var actionpending = $("#actionpending").html();
+  if ( actionpending == 1 ) {
+       return;
+  }
+  var actionpending = $("#actionpending");
+  actionpending.html('1');
+  if ( actionpending == 1 ) {
+   return;
+  }
+  var refreshpending = $("#refreshpending").html();
+  if ( refreshpending == 1 ) {
+       return;
+  }
+  var refreshpending = $("#refreshpending");
+  refreshpending.html('1');
 
   $.ajax({  
         type: "POST",
@@ -11,6 +26,7 @@ setInterval(function(){
             //$("#allvms").hide();
             $("#allvms").html(data);
             $("#allvms").show(400);
+	    $("#refreshpending").replaceWith('<div id="refreshpending" class="hidden">0</div>');
 		}
 	});
 },3000);
