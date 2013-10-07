8911161102a5d86b767fa6a2754501625af90cbb
karmab <karimboumedhel@gmail.com>
2013-10-07 08:48:20 +0200

    associate ldap filters to groups. bet wont work at first try :)

--- portal/auth.py
+++ portal/auth.py
@@ -17,7 +17,11 @@ class LdapBackend(object):
 		ldapproviders=LdapProvider.objects.all()
 		for provider in ldapproviders:
 			port = 389
-			host,basedn,binddn,bindpassword,secure,userfield,filter1,certname=provider.host,provider.basedn,provider.binddn,provider.bindpassword,provider.secure,provider.userfield,provider.filter1,provider.certname
+			host, basedn, binddn, bindpassword, secure, userfield,certname, filter1, filter2, filter3, filter4, groups1, groups2, groups3, groups4 = provider.host, provider.basedn, provider.binddn, provider.bindpassword, provider.secure, provider.userfield, provider.certname, provider.filter1, provider.filter2, provider.filter3, provider.filter4, provider.groups1, provider.groups2, provider.groups3, provider.groups4
+			activefilters=[]
+			for filter in [filter1,filter2,filter3,filter4]:
+				if filter:
+					activefilters.append(filter)
 			ldapuri="ldap://%s" % (host)
 			if secure:
 				port = 636
@@ -34,13 +38,19 @@ class LdapBackend(object):
 				print "unreachable host %s, continuing" % host
 				continue
 			userfilter = "%s=%s" % (userfield,username)
-			if filter1:
-				userfilter="(&(%s)(%s))" % (userfilter,filter1)
-			attrs = [str(userfield)]
 			try:
 				c = ldap.initialize(ldapuri)
 				c.simple_bind_s(binddn,bindpassword)
-				res=c.search_s( basedn, ldap.SCOPE_SUBTREE, userfilter, attrs)
+				if not activefilters:
+					res=c.search_s( basedn, ldap.SCOPE_SUBTREE, userfilter, attrs)
+				else:
+					for filter in activefilters:
+						userfilter="(&(%s)(%s))" % (userfilter,filter)
+						attrs = [str(userfield)]
+						res=c.search_s( basedn, ldap.SCOPE_SUBTREE, userfilter, attrs)
+						if res[0]:
+							activefilters = filter
+							break
 				c.unbind()
 			except:	
 				continue
@@ -61,10 +71,26 @@ class LdapBackend(object):
 			user, created = User.objects.get_or_create(username=username)
 			if created:
 				print "created %s" % username
-				if len(provider.groups1.values()) >=1 and not provider.filter1:
-					for group in provider.groups1.values():
+				if groups1.values() and not filter1:
+					for group in groups1.values():
 						user.groups.add(group['id'])
-				user.save()
+					user.save()
+				if filter and filter==filter1 and groups1.values():
+					for group in groups1.values():
+						user.groups.add(group['id'])
+					user.save()
+				if filter and filter==filter2 and groups2.values():
+					for group in groups2.values():
+						user.groups.add(group['id'])
+					user.save()
+				if filter and filter==filter3 and groups3.values():
+					for group in groups3.values():
+						user.groups.add(group['id'])
+					user.save()
+				if filter and filter==filter4 and groups4.values():
+					for group in groups4.values():
+						user.groups.add(group['id'])
+					user.save()
 			return user
 
 	def get_user(self, user_id):
