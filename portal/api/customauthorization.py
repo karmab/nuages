784e80c483bcd7807cab19caaa9b61f35bfc1cef
karmab <karimboumedhel@gmail.com>
2014-04-25 03:29:22 +0200

    restablished DjangoAuth as it s blocking custom REST calls

--- portal/api/customauthorization.py
+++ portal/api/customauthorization.py
@@ -4,59 +4,56 @@ from tastypie.exceptions import Unauthorized
 class StaffAuthorization(Authorization):
     def read_list(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return object_list
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).read_list(object_list, bundle)
 
     def read_detail(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return True
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).read_detail(object_list, bundle)
         
-
     def create_list(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return object_list
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).create_list(object_list, bundle)
 
     def create_detail(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return True
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).create_detail(object_list, bundle)
 
     def update_list(self, object_list, bundle):
         user = bundle.request.user
         if not user.is_staff:
             raise Unauthorized("")
-        allowed = []
-        for obj in object_list:
-            allowed.append(obj)
-        return allowed
+        else:
+            super(StaffAuthorization,self).update_list(object_list, bundle)
 
     def update_detail(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return True
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).update_detail(object_list, bundle)
 
     def delete_list(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return object_list
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).delete_list(object_list, bundle)
 
     def delete_detail(self, object_list, bundle):
         user = bundle.request.user
-        if user.is_staff:
-            return object_list
-        else:
+        if not user.is_staff:
             raise Unauthorized("")
+        else:
+            super(StaffAuthorization,self).delete_detail(object_list, bundle)
